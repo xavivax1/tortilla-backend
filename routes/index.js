3d0b967f79c65ff4239c598eaf070ccbd9dd39eb
Filipe Rainho <filipenrainho@gmail.com>
2019-03-01 17:21:55 +0100

    finish crud

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,58 @@ const router = express.Router();
 const Tortilla = require('../models/Tortilla');
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  try {
+    const tortillas = await Tortilla.find();
+    res.render('list', { tortillas });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get('/new', (req, res, next) => {
-  res.render('create');
+  res.render('create-edit');
 });
 
-router.post('/new', async (req, res, next) => {
-  const newTortilla = req.body;
+router.post('/', async (req, res, next) => {
+  const { _id, name, special, size } = req.body;
+  const tortilla = { name, special, size };
   try {
-    const result = await Tortilla.create(newTortilla);
+    if (_id) {
+      await Tortilla.findByIdAndUpdate(_id, tortilla);
+    } else {
+      await Tortilla.create(tortilla);
+    }
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/tortillas/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const tortilla = await Tortilla.findById(id);
+    res.render('detail', tortilla);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/tortillas/:id/edit', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const tortilla = await Tortilla.findById(id);
+    res.render('create-edit', tortilla);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/tortillas/:id/delete', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    await Tortilla.findByIdAndDelete(id);
     res.redirect('/');
   } catch (error) {
     next(error);
