d8ee147299b1beb0eae2c021d8ece7ce69ae4c5c
Filipe Rainho <filipenrainho@gmail.com>
2019-03-06 15:51:37 +0100

    change model order

--- models/User.js
+++ models/User.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
+  password: {
     type: String,
     required: true
   },
-  password: {
+  username: {
     type: String,
     required: true
   }
